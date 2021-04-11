25e929510e5ea07a3c9d36c16defd76ec4479f8a
6035512066 <pdtry5698@gmail.com>
2021-04-11 16:52:43 +0700

    commit

--- lib/screens_kathu/information_kathu/place/widget/kbutton2.dart
+++ lib/screens_kathu/information_kathu/place/widget/kbutton2.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project/constant.dart';
 import 'package:project/screens_kathu/information_kathu/place/widget/kmap2.dart';
-
+import 'package:project/screens_kathu/information_kathu/place/widget/reviewkp2/screens/home_page.dart';
 
 
 
@@ -68,7 +68,7 @@ class kButton_place2 extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      //return LoginScreen();
+                      return PageHomekp2();
                       //return LoginPage();
                       //return Login3();
                     },
