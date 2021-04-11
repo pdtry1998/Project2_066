31a5f00b02b74597d45665c920cf4653b86ea50b
6035512066 <pdtry5698@gmail.com>
2021-04-11 21:23:50 +0700

    commit

--- lib/screens_Thalang/information_thalang/market/widget/tbuttonmarket1.dart
+++ lib/screens_Thalang/information_thalang/market/widget/tbuttonmarket1.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project/constant.dart';
 import 'package:project/screens_Thalang/information_thalang/market/widget/tmapmarket1.dart';
-
+import 'package:project/screens_Thalang/information_thalang/market/widget/reviewtm1/screens/home_page.dart';
 
 
 
@@ -68,7 +68,7 @@ class tButton_Market1 extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      //return LoginScreen();
+                      return PageHometm1();
                       //return LoginPage();
                       //return Login3();
                     },
