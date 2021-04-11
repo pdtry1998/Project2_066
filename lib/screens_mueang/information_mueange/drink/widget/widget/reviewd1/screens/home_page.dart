39581ee56505ae73cdfaddb38096f951e395e04e
6035512066 <pdtry5698@gmail.com>
2021-04-11 15:35:00 +0700

    commit

---
+++ lib/screens_mueang/information_mueange/drink/widget/widget/reviewd1/screens/home_page.dart
@@ -0,0 +1,87 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:project/screens_mueang/information_mueange/drink/widget/widget/reviewd1/screens/add_item_page.dart';
+import 'package:project/screens_mueang/information_mueange/drink/widget/widget/reviewd1/screens/item_page.dart';
+
+
+class PageHomed1 extends StatefulWidget {
+  _PageHomed1State createState() => _PageHomed1State();
+}
+
+class _PageHomed1State extends State<PageHomed1> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("รีวิว"),
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection("catalogd1").snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  Text("Loading . . . "),
+                ],
+              ),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Container(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(  // ส่วนลิ้งไปหน้าถัดไป
+                                  builder: (context) => ItemPaged1(
+                                    pdname: snapshot
+                                        .data.documents[index].documentID,
+                                    pddescription: snapshot
+                                        .data
+                                        .documents[index]
+                                        .data["description"],
+                                  ))
+                          );
+                        },
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                              title:
+                              Text(
+                                  snapshot.data.documents[index].documentID),
+                              subtitle:
+                              Text(
+                                  snapshot.data.documents[index].data["description"]),
+
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => AddItemPaged1()),);},
+        child: Icon(Icons.border_color),
+      ),
+    );
+  }
+}
