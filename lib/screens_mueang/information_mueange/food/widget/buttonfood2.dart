eafe32178a5588a8b1b70b83ba37219ce340e437
6035512066 <pdtry5698@gmail.com>
2021-04-11 12:00:57 +0700

    commit

--- lib/screens_mueang/information_mueange/food/widget/buttonfood2.dart
+++ lib/screens_mueang/information_mueange/food/widget/buttonfood2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project/constant.dart';
+import 'package:project/screens_mueang/information_mueange/food/widget/reviewf2/screens/home_page.dart';
 import 'package:project/screens_mueang/information_mueange/market/widget/mapmarket2.dart';
 
 
@@ -65,7 +66,7 @@ class ButtonFood2 extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      //return LoginScreen();
+                      return PageHomef2();
                       //return LoginPage();
                       //return Login3();
                     },
