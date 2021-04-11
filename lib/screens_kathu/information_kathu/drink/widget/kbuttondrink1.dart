337d0c7798834731955ad7fc6050a39d91a92f21
6035512066 <pdtry5698@gmail.com>
2021-04-11 20:15:35 +0700

    commit

--- lib/screens_kathu/information_kathu/drink/widget/kbuttondrink1.dart
+++ lib/screens_kathu/information_kathu/drink/widget/kbuttondrink1.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project/constant.dart';
 import 'package:project/screens_kathu/information_kathu/drink/widget/kmapdrink1.dart';
-
+import 'package:project/screens_kathu/information_kathu/drink/widget/reviewkd1/screens/home_page.dart';
 
 
 class kButton_Drink1 extends StatelessWidget {
@@ -64,7 +64,7 @@ class kButton_Drink1 extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      //return LoginScreen();
+                      return PageHomekd1();
                       //return LoginPage();
                       //return Login3();
                     },
---
+++ lib/screens_kathu/information_kathu/drink/widget/reviewkd1/screens/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:project/screens_kathu/information_kathu/drink/widget/reviewkd1/screens/add_item_page.dart';
+import 'package:project/screens_kathu/information_kathu/drink/widget/reviewkd1/screens/item_page.dart';
+
+class PageHomekd1 extends StatefulWidget {
+  _PageHomekd1State createState() => _PageHomekd1State();
+}
+
+class _PageHomekd1State extends State<PageHomekd1> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("รีวิว"),
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection("catalogkd1").snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  Text("Loading . . . "),
+                ],
+              ),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Container(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(  // ส่วนลิ้งไปหน้าถัดไป
+                                  builder: (context) => ItemPagekd1(
+                                    pdname: snapshot
+                                        .data.documents[index].documentID,
+                                    pddescription: snapshot
+                                        .data
+                                        .documents[index]
+                                        .data["description"],
+                                  ))
+                          );
+                        },
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                              title:
+                              Text(
+                                  snapshot.data.documents[index].documentID),
+                              subtitle:
+                              Text(
+                                  snapshot.data.documents[index].data["description"]),
+
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => AddItemPagekd1()),);},
+        child: Icon(Icons.border_color),
+      ),
+    );
+  }
+}
