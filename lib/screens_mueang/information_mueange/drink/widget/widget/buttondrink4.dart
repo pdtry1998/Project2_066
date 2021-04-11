1b5b58d09ed17051e4324a394691b4521ae0f82a
6035512066 <pdtry5698@gmail.com>
2021-04-11 16:00:58 +0700

    commit

--- lib/screens_mueang/information_mueange/drink/widget/widget/buttondrink4.dart
+++ lib/screens_mueang/information_mueange/drink/widget/widget/buttondrink4.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project/constant.dart';
 import 'package:project/screens_mueang/information_mueange/drink/widget/widget/mapdrink4.dart';
-
+import 'package:project/screens_mueang/information_mueange/drink/widget/widget/reviewd4/screens/home_page.dart';
 
 
 
@@ -65,7 +65,7 @@ class ButtonDrink4 extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      //return LoginScreen();
+                      return PageHomed4();
                       //return LoginPage();
                       //return Login3();
                     },
